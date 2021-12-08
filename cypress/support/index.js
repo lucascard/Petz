b25c9b33851be82793e00530b63ed57d83167cbd
Lucro <cardosolucasrodrigues@gmail.com>
2021-12-08 16:48:36 -0300

    Adicionando extensão para retirar as requests do log no cypress

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -22,3 +22,22 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   // returning false here prevents Cypress from failing the test
   return false
 })
+
+Cypress.Server.defaults({
+  delay:500,
+  force404: false,
+  ignore: (xhr) => {
+    return true;
+  }
+})
+
+const app = window.top;
+
+if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
+  const style = app.document.createElement('style');
+  style.innerHTML =
+    '.command-name-request, .command-name-xhr { display: none }';
+  style.setAttribute('data-hide-command-log-request', '');
+
+  app.document.head.appendChild(style);
+}
